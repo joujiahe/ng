79b648d938b40224a4c39e201fa0121afb685d94
Johnson Jou <johnson@unispace3d.com>
2013-09-24 17:47:48 +0800

    init
    Signed-off-by: Johnson Jou <johnson@unispace3d.com>

---
+++ app/app.js
@@ -0,0 +1,14 @@
+define( ['angular'],
+function( angular ){
+
+'use strict';
+
+return angular.module('app', [])
+
+.config([function() {
+}])
+
+.run([function() {
+}]);
+
+});
---
+++ app/main.js
@@ -0,0 +1,23 @@
+require.config({
+    //By default load any module IDs from js
+    baseUrl: 'app',
+    //except, if the module ID starts with "app",
+    //load it from the js/app directory. paths
+    //config is relative to the baseUrl, and
+    //never includes a ".js" extension since
+    //the paths config could be for a directory.
+    paths: {
+        angular: 'vendor/angular.min'
+    },
+    shim: {
+        angular: {
+            exports: 'angular'
+        }
+    }
+});
+
+// Start the main app logic.
+require(['angular', 'app'],
+function( angular ,  app ){
+    angular.bootstrap(document, ['app']);
+});
